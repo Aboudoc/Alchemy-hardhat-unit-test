1a50baf42b3f06bd296ba82eb920b893ad5a2bcb
Aboudoc <aboutika.reda@gmail.com>
2022-12-27 08:34:12 +0100

    initial commit

--- test/Faucet.js
+++ test/Faucet.js
@@ -1,16 +1,17 @@
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("Faucet", function () {
   async function deployContractAndSetVariables() {
     const Faucet = await ethers.getContractFactory("Faucet");
     const faucet = await Faucet.deploy();
 
-    const [owner] = await ethers.getSigners();
+    const [owner, hacker] = await ethers.getSigners();
     let withdrawAmount = ethers.utils.parseUnits("0.01", "ether");
 
     console.log("Signer 1 address: ", owner.address);
-    return { faucet, owner, withdrawAmount };
+    return { faucet, owner, hacker, withdrawAmount };
   }
 
   it("should deploy and set the owner correctly", async function () {
@@ -25,5 +26,17 @@ describe("Faucet", function () {
     );
 
     await expect(faucet.withdraw(withdrawAmount)).to.be.reverted;
+  });
+
+  it("only the owner should withdraw all", async function () {
+    const { faucet, hacker } = await loadFixture(deployContractAndSetVariables);
+
+    await expect(faucet.connect(hacker).withdrawAll()).to.be.reverted;
+  });
+
+  it("only the owner should destroy the faucet", async function () {
+    const { faucet, hacker } = await loadFixture(deployContractAndSetVariables);
+
+    await expect(faucet.connect(hacker).destroyFaucet()).to.be.reverted;
   });
 });
